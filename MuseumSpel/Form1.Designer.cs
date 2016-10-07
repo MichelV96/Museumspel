188e9795f5c7e033468f2dd174d9412415517c69
Jordy van den Berg <jordyberg@gmail.com>
2016-10-07 10:16:10 +0200

    NameChange fix

--- MuseumSpel/Form1.Designer.cs
+++ MuseumSpel/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.Text = "Museum Game";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
 
         }
--- MuseumSpel/Speler.cs
+++ MuseumSpel/Speler.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace MuseumSpel
 {
+    // Model
     public class Speler : SpelObject
     {
         public int speed { get; set; }
