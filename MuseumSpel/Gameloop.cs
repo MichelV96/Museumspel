bf9158058bcd33c0ae2306391c1027efc5246f59
Nick Kronenberg <nikro412956@gmail.com>
2016-10-10 11:14:11 +0200

    onnodige comments verwijderd

--- MuseumSpel/Gameloop.cs
+++ MuseumSpel/Gameloop.cs
@@ -34,50 +34,23 @@ namespace MuseumSpel
         public void gameLoop()
         {
             Console.WriteLine("GameLoop got called");
-            //DataReset();
-            //Game();
-            //Menu();
-            //Form form = (Form)this;
-            //timer.Start();
-            //Nick = 0;
-
-            //while (!p_gameOver)
-            //{
-
-            //if (!menuSeen)
-            //{
-            //    Console.WriteLine("menu not seen");
-            //    Game();
-            //    Menu();
-            //}
-
-
-            //startTime = timer.ElapsedMilliseconds;
-            //Game();
-            //Application.DoEvents();
+            
             //update timer
 
             p_currentTime = Environment.TickCount;
-            //Console.WriteLine(p_currentTime.ToString()+" current");
-            //Console.WriteLine(p_startTime.ToString()+" start");
+            
             //refresh at 60 FPS
-            if (p_currentTime > p_startTime + 25)// moet 16 zijn
+            if (p_currentTime > p_startTime + 16)
             {
                 framecounter2++;
                 if (p_currentTime > frameTimer + 1000)
                 {
                     framerate2 = framecounter2;
                     framecounter2 = 0;
-                    //Console.WriteLine(framerate2.ToString());
                 }
-                //Console.WriteLine("hihihihihihihihihihihihihihihihihihihihihihihihihihihihihihi");
                 //update timing
                 p_startTime = p_currentTime;
-                //Console.WriteLine("hohohohohohohohohohohohohohohohohohohohohohohohohohohohohohoho");
-                //give the form some cycles
-                //Game_Draw();
-                //GuardIdleActions();
-                //Guard();
+                //give the form a cycle
                 if (ModelChanged != null)
                 {
                     ModelChanged();
@@ -105,7 +78,6 @@ namespace MuseumSpel
 
                 frameCount = 0;
             }
-            //}
 
         }
 
