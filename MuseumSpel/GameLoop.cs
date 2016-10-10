f957f876f2bc473c7aec542e82087d5a3b7545b4
louwrens <louwrensjr@hotmail.com>
2016-10-10 16:01:38 +0200

    no message

---
+++ MuseumSpel/GameLoop.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuseumSpel
+{
+    public class GameLoop
+    {
+        private int p_startTime = 0;
+        public int guardTime = 0;
+        public int p_currentTime = 0;
+        public bool p_gameOver = false;
+        public int frameCount = 0;
+        public int frameTimer = 0;
+        public float frameRate = 0;
+        public int seconds = 0;
+        public int minutes = 0;
+        public int hours = 0;
+        public int score = 0;
+        public int framecounter2 = 0;
+        public int framerate2 = 0;
+        private string time;
+
+        public event ModelChangedEventHandeler ModelChanged; // wanneer je de View aanroepen doe je: ModelChanged();
+
+        public void ShutDown()
+        {
+
+            p_gameOver = true;
+            Console.WriteLine("It's over man gameover: gg wp");
+        }
+
+        public void redraw()
+        {
+            if (ModelChanged != null)
+            {
+                ModelChanged();
+            }
+        }
+
+        public void gameLoop()
+        {
+            //update timer
+
+            p_currentTime = Environment.TickCount;
+            
+            //refresh at 60 FPS
+            if(p_currentTime > guardTime + 33)
+            {
+                // guard delegate JORDY NU!!!!
+            }
+            if (p_currentTime > p_startTime + 16)
+            {
+                framecounter2++;
+                if (p_currentTime > frameTimer + 1000)
+                {
+                    framerate2 = framecounter2;
+                    framecounter2 = 0;
+                }
+                //update timing
+                p_startTime = p_currentTime;
+                //give the form a cycle
+                if (ModelChanged != null)
+                {
+                    ModelChanged();
+                }
+            }
+            frameCount++;
+            if (p_currentTime > frameTimer + 1000)
+            {
+                seconds++;
+
+                if (seconds == 60)
+                {
+                    minutes++;
+                    seconds = 0;
+                }
+                if (minutes == 60)
+                {
+                    hours++;
+                    minutes = 0;
+                }
+                time = hours.ToString() + ":" + minutes.ToString() + ":" + seconds.ToString();
+                Console.WriteLine(time);
+                frameTimer = p_currentTime;
+                frameRate = frameCount;
+
+                frameCount = 0;
+            }
+
+        }
+
+    }
+}
---
+++ MuseumSpel/SpeelVeldController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MuseumSpel
+{
+    // Controller voor speelveld
+    public class SpeelVeldController
+    {
+        private SpeelVeld speelVeld; // model
+        private Form1 form1; //view
+
+        public SpeelVeldController(Form1 form1, SpeelVeld speelVeld)
+        {
+            this.speelVeld = speelVeld;
+            this.form1 = form1;
+        }
+
+        public void OnKeyPressed(KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.W)
+            {
+                speelVeld.setRichting(Direction.Up);
+            }
+            else if (e.KeyData == Keys.S)
+            {
+                speelVeld.setRichting(Direction.Down);
+            }
+            else if (e.KeyData == Keys.A)
+            {
+                speelVeld.setRichting(Direction.Left);
+            }
+            else if (e.KeyData == Keys.D)
+            {
+                speelVeld.setRichting(Direction.Right);
+            }
+            if (e.KeyData == Keys.F)
+            {
+                speelVeld.pakSchilderij(true);
+            }
+            form1.Invalidate();
+        }
+
+        public void OnKeyUp(KeyEventArgs e)
+        {
+            this.speelVeld.idle = true;
+            if (e.KeyData == Keys.W)
+            {
+                speelVeld.speler.setPicture(Direction.UpIdle);
+            }
+            else if (e.KeyData == Keys.S)
+            {
+                speelVeld.speler.setPicture(Direction.DownIdle);
+            }
+            else if (e.KeyData == Keys.A)
+            {
+                speelVeld.speler.setPicture(Direction.LeftIdle);
+            }
+            else if (e.KeyData == Keys.D)
+            {
+                speelVeld.speler.setPicture(Direction.RightIdle);
+            }
+            form1.Invalidate();
+        }
+
+    }
+}
