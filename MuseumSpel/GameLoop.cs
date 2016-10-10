52bfbbb3914ca8f108b654f510693d41ab66c6f8
Nick Kronenberg <nikro412956@gmail.com>
2016-10-10 14:34:57 +0200

    no message

--- MuseumSpel/GameLoop.cs
+++ MuseumSpel/GameLoop.cs
@@ -9,7 +9,7 @@ namespace MuseumSpel
     public class GameLoop
     {
         private int p_startTime = 0;
-        private int p_currentTime = 0;
+        public int p_currentTime = 0;
         public bool p_gameOver = false;
         public int frameCount = 0;
         public int frameTimer = 0;
@@ -26,14 +26,22 @@ namespace MuseumSpel
 
         public void ShutDown()
         {
-            
+
             p_gameOver = true;
             Console.WriteLine("it's over man gameover gg");
         }
 
+        public void redraw()
+        {
+            if (ModelChanged != null)
+            {
+                ModelChanged();
+            }
+        }
+
         public void gameLoop()
         {
-            Console.WriteLine("GameLoop got called");
+           // Console.WriteLine("GameLoop got called");
             //DataReset();
             //Game();
             //Menu();
@@ -44,71 +52,63 @@ namespace MuseumSpel
             //while (!p_gameOver)
             //{
 
-                //if (!menuSeen)
-                //{
-                //    Console.WriteLine("menu not seen");
-                //    Game();
-                //    Menu();
-                //}
+            //if (!menuSeen)
+            //{
+            //    Console.WriteLine("menu not seen");
+            //    Game();
+            //    Menu();
+            //}
 
 
-                //startTime = timer.ElapsedMilliseconds;
-                //Game();
-                //Application.DoEvents();
-                //update timer
+            //startTime = timer.ElapsedMilliseconds;
+            //Game();
+            //Application.DoEvents();
+            //Console.WriteLine("GameLoop got called");
+            //update timer
 
-                p_currentTime = Environment.TickCount;
-                //Console.WriteLine(p_currentTime.ToString()+" current");
-                //Console.WriteLine(p_startTime.ToString()+" start");
-                //refresh at 60 FPS
-                if (p_currentTime > p_startTime + 25)// moet 16 zijn
+            p_currentTime = Environment.TickCount;
+            
+            //refresh at 60 FPS
+            if (p_currentTime > p_startTime + 16)
+            {
+                framecounter2++;
+                if (p_currentTime > frameTimer + 1000)
                 {
-                    framecounter2++;
-                    if (p_currentTime > frameTimer + 1000)
-                    {
-                        framerate2 = framecounter2;
-                        framecounter2 = 0;
-                        //Console.WriteLine(framerate2.ToString());
-                    }
-                    //Console.WriteLine("hihihihihihihihihihihihihihihihihihihihihihihihihihihihihihi");
-                    //update timing
-                    p_startTime = p_currentTime;
-                //Console.WriteLine("hohohohohohohohohohohohohohohohohohohohohohohohohohohohohohoho");
-                //give the form some cycles
-                //Game_Draw();
-                //GuardIdleActions();
-                //Guard();
+                    framerate2 = framecounter2;
+                    framecounter2 = 0;
+                }
+                //update timing
+                p_startTime = p_currentTime;
+                //give the form a cycle
                 if (ModelChanged != null)
                 {
                     ModelChanged();
                 }
-                }
-                frameCount++;
-                if (p_currentTime > frameTimer + 1000)
+            }
+            frameCount++;
+            if (p_currentTime > frameTimer + 1000)
+            {
+                seconds++;
+
+                if (seconds == 60)
                 {
-                    seconds++;
-
-                    if (seconds == 60)
-                    {
-                        minutes++;
-                        seconds = 0;
-                    }
-                    if (minutes == 60)
-                    {
-                        hours++;
-                        minutes = 0;
-                    }
-                    time = hours.ToString() + ":" + minutes.ToString() + ":" + seconds.ToString();
-                    Console.WriteLine(time);
-                    frameTimer = p_currentTime;
-                    frameRate = frameCount;
-
-                    frameCount = 0;
+                    minutes++;
+                    seconds = 0;
                 }
-            //}
+                if (minutes == 60)
+                {
+                    hours++;
+                    minutes = 0;
+                }
+                time = hours.ToString() + ":" + minutes.ToString() + ":" + seconds.ToString();
+                Console.WriteLine(time);
+                frameTimer = p_currentTime;
+                frameRate = frameCount;
+
+                frameCount = 0;
+            }
 
         }
 
     }
 }
-
