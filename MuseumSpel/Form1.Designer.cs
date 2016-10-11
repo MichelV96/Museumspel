41e502465899eeef8b7e148c6a31e187d03f04ad
Jordy van den Berg <jordyberg@gmail.com>
2016-10-11 11:01:34 +0200

    Pretty

--- MuseumSpel/Form1.Designer.cs
+++ MuseumSpel/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1507, 701);
+            this.ClientSize = new System.Drawing.Size(1132, 677);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Museum Game";
