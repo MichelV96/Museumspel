16b94e0622b12a0d2763acb029ab4afd9e6178b4
Jordy van den Berg <jordyberg@gmail.com>
2016-10-26 09:49:09 +0200

    Refresh naar invaldidate -> geeft minder lagg

--- MuseumSpel/Form1.cs
+++ MuseumSpel/Form1.cs
@@ -54,7 +54,8 @@ namespace MuseumSpel
                 speelVeld.SpelerMovement(Direction.Left);
 
             Application.DoEvents();
-            this.Refresh();// Heel speelveld wordt opnieuw getekend
+            //this.Refresh();// Heel speelveld wordt opnieuw getekend
+            Invalidate();
 
         }
         public void shuttingUp()
